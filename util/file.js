ce87f12aa180b45d666142feed62c976656c9767
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-04-11 12:32:08 +0530

    Added user status and uodate user status

--- util/file.js
+++ util/file.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const fs = require('fs');
 
-const clearImage = (filePath) => {
+const clearImage = filePath => {
   filePath = path.join(__dirname, '..', filePath);
-  fs.unlink(filePath, (err) => console.log(err));
+  fs.unlink(filePath, err => console.log(err));
 };
 
 exports.clearImage = clearImage;
